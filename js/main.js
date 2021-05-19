5c41ebccbe9395e17d584e19b41cb594a552db03
Artem <artyom.kalyuzhin@gmail.com>
2021-05-19 22:27:06 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let c = 10;
+console.log(c);
+
+const obj = {
+    a: 5
+};
+obj.a = 5;
+
+console.log(obj);
+
+console.log(b);
+var b = 7;
+
+console.log(+prompt("Prompt a number:"));
